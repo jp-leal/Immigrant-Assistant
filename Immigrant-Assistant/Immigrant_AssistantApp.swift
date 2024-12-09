93ddfa773fb8b6392434f785da3e5884892ea8b3
joaobug <joaoenfrance@gmail.com>
2024-12-09 10:58:12 -0300

    Initial Commit

---
+++ Immigrant-Assistant/Immigrant_AssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  Immigrant_AssistantApp.swift
+//  Immigrant-Assistant
+//
+//  Created by Joao Leal on 12/9/24.
+//
+
+import SwiftUI
+
+@main
+struct Immigrant_AssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
